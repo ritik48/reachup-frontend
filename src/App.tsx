60968f065b50a869c6681d8843396a5cdccbd9a4
Ritik <raj76941@gmail.com>
2025-04-02 17:20:21 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home/home";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
