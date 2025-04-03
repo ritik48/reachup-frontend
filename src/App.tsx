724bbb91e28380da969b0234fe9b0f5dbd447b53
Ritik <raj76941@gmail.com>
2025-04-03 23:02:58 +0530

    feat: added user provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Layout from "./components/Layout";
 import { Leads } from "./pages/Leads";
 import { Connect } from "./pages/Connect/Connect";
 import { ProtectedRoute } from "./components/ProtectedRoutes";
+import { UserProvider } from "./contexts/UserContext";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/leads" element={<Leads />} />
-            <Route path="/connect" element={<Connect />} />
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/leads" element={<Leads />} />
+              <Route path="/connect" element={<Connect />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 }
