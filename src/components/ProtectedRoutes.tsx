b10157735d14459cff88f8a456c68254b09b8e78
Ritik <raj76941@gmail.com>
2025-04-04 17:44:25 +0530

    show loading while checking auth

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,8 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "@/contexts/UserContext";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export function ProtectedRoute() {
-  const { user } = useUser();
+  const { user, loading } = useUser();
+
+  if (loading) {
+    return <ClipLoader size={15} color="grey" />;
+  }
 
   if (!user) {
     return <Navigate to="/" replace />;
